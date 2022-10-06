127226ff3b2b6d7d8528db9ad811b84706b4e5cf
Anthony Chiado <anthonychiado97@gmail.com>
2022-10-06 16:48:53 -0400

    Begin data prep

--- fifa_wc_script.R
+++ fifa_wc_script.R
@@ -1,1 +1,12 @@
+## STETUP ----------
 library(tidyverse)
+
+international_matches <- read.csv(file = "~/Desktop/work/data/r/kaggle/fifa_wc_2022/international_matches.csv")
+
+
+## DATA PREP ----------
+wc_data <- international_matches %>%
+  filter(date >= '2006-01-01', tournament %in% c("AFC Asian Cup", "African Cup of Nations", "CONCACAF Nations League", "Gold Cup",
+                                                 "Copa América", "FIFA World Cup", "FIFA World Cup qualification", "UEFA Euro",
+                                                 "UEFA Euro qualification", "UEFA Nations League"))
+
